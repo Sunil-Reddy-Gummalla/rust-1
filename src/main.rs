d08efeb56bd1b6fe795c47433048a78115035ee6
Venkata Sunil Reddy Gummalla <gummalla.sunilreddy@cvnio.com>
2025-10-06 17:25:49 +0400

    feat: add CLI number guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
+use rand::Rng;
+use std::io;
+
 fn main() {
-    let mut sentence = String::from("My Hello, world!");
-    for _ in 0..100 {
-        sentence.push_str("hgshgvgsvgsvgsvgvs");
-        println!("{:p}", sentence.as_ptr());
+    println!("Guess the number! ");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number!");
+                continue;
+            }
+        };
+        println!("You guessed: {guess}");
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("Too small!"),
+            std::cmp::Ordering::Greater => println!("Too big!"),
+            std::cmp::Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
