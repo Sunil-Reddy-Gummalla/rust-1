4e2f367f7db811bfc46a520998c352bf84861e81
Venkata Sunil Reddy Gummalla <gummalla.sunilreddy@cvnio.com>
2025-10-13 09:15:02 +0400

    practice hashmap pattern match

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use std::collections::HashMap;
+
+
 fn main() {
-    let mut sentence = String::from("My Hello, world!");
-    for _ in 0..100 {
-        sentence.push_str("hgshgvgsvgsvgsvgvs");
-        println!("{:p}", sentence.as_ptr());
+
+  let mut map = HashMap::new();
+  map.insert("key1", "value1");
+  map.insert("key2", "value2");
+
+  let first_value = map.get("key1");
+  match first_value {
+    Some(value) => println!("Found value: {}", value),
+    None => {
+      println!("'key1' not found, inserting default value.");
     }
+  }
+  println!("The value for 'key1' is: {:?}", first_value);
+
 }
