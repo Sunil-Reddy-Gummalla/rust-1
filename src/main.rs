72ef920c330b90204bb70ec7baa175b78b8af24e
Venkata Sunil Reddy Gummalla <gummalla.sunilreddy@cvnio.com>
2025-10-13 13:03:40 +0400

    feat: Implement multi-threaded sum calculation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
+// find the sum from 1 to 10^8 using threads
+
+use::std::{
+    sync::mpsc,
+    thread::{self, spawn}
+};
+
 fn main() {
-    let mut sentence = String::from("My Hello, world!");
-    for _ in 0..100 {
-        sentence.push_str("hgshgvgsvgsvgsvgvs");
-        println!("{:p}", sentence.as_ptr());
+    let (tx, rx) = mpsc::channel();
+    for i in 0..10 {
+        let producer = tx.clone();
+        spawn(move || {
+            let mut sum: u64 = 0;
+            for j in i * 10000000..(i + 1) * 10000000 {
+                sum = sum + j;
+            }
+            producer.send(sum).unwrap();
+        });
     }
+    drop(tx); // close the channel
+    let mut final_sum = 0;
+    for val in rx {
+        println!("Received: {}", val);
+        final_sum = final_sum + val;
+    }
+    println!("Final sum: {}", final_sum);
+    
 }
